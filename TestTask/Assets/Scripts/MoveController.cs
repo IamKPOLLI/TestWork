4e773fc8af7c0b698793d9f82677c68ed57041ca
IamKPOLLI <aleksdavydoveu@gmail.com>
2021-01-21 23:16:53 +0300

    Foundation

---
+++ TestTask/Assets/Scripts/MoveController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private Text _speedLabel;
+    [SerializeField] private Camera _camera;
+    public Vector2 startMarker;
+    public Vector2 endMarker;
+    public float speed;
+    private Queue<Vector2> _points; 
+   
+
+    
+
+    
+
+
+
+    void Start()
+    {
+        ChangeSpeed(1f);
+        startMarker = transform.position;
+        endMarker = transform.position;
+        _points = new Queue<Vector2>();
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           _points.Enqueue(_camera.ScreenToWorldPoint(Input.mousePosition));
+        }
+
+
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, endMarker, step);
+
+        if(transform.position.x == endMarker.x && transform.position.y == endMarker.y && _points.Count != 0)
+        {
+            endMarker = _points.Dequeue();
+        }
+    }
+
+   public void ChangeSpeed(float new_speed)
+    {
+        speed = new_speed;
+        _speedLabel.text = new_speed.ToString();
+    }
+
+}
